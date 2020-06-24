76b73718b83f27b74d88dbf9607eb090b52eae25
divcraft <t.ogonowski@wp.pl>
2020-06-24 19:13:24 +0200

    create add-news site

--- routes/admin.js
+++ routes/admin.js
@@ -15,22 +15,36 @@ router.all('*', (req, res, next) => {
 })
 
 router.get('/', (req, res, next) => {
-   const newsData = new News({
-      title: 'Tytuł testowy',
-      description: 'Opis testowy'
-   })
-
-   newsData.save((err) => {
-      if (err) {
-         console.log(err)
-      } else {
-         console.log('Połączono z kolekcją News')
-      }
-   })
-
-   res.render('admin', {
+   // const newsData = new News({
+   //    title: 'Tytuł testowy',
+   //    description: 'Opis testowy'
+   // })
+   // newsData.save((err) => {
+   //    if (err) {
+   //       console.log(err)
+   //    } else {
+   //       console.log('Połączono z kolekcją News')
+   //    }
+   // })
+   res.render('admin/index', {
       title: 'Admin'
    })
+})
+
+router.get('/news/add', (req, res) => {
+   res.render('admin/news-form', {
+      title: 'Dodaj news'
+   })
+})
+
+router.post('/news/add', (req, res) => {
+   const {
+      newsTitle,
+      newsDescription
+   } = req.body
+   console.log(newsTitle)
+   console.log(newsDescription)
+   res.redirect('/admin/news/add')
 })
 
 module.exports = router
