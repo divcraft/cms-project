14a1d9b306b44901ae598c1b3d51cc782d136531
divcraft <t.ogonowski@wp.pl>
2020-06-25 19:35:59 +0200

    create a quiz site

--- routes/quiz.js
+++ routes/quiz.js
@@ -1,9 +1,33 @@
 const express = require('express')
+const Quiz = require('../models/quiz')
+
 const router = express.Router()
 
-router.get('/', (req, res, next) => {
-   res.render('quiz', {
-      title: 'Quiz'
+router.get('/', (req, res) => {
+   const {
+      hasVoted
+   } = req.session
+   Quiz.find({}, (err, data) => {
+      console.log(data)
+      res.render('quiz', {
+         title: 'Quiz',
+         data,
+         hasVoted
+      })
+   })
+
+})
+
+router.post('/', (req, res) => {
+   const {
+      quiz
+   } = req.body
+   Quiz.findById(quiz, (err, data) => {
+      data.vote++
+      data.save((err) => {
+         req.session.hasVoted = true
+         res.redirect('/quiz')
+      })
    })
 })
 
